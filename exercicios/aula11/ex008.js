1b3d4f06770a9bd33f44d752971100f1747433d5
Matheus de Assis Zordan <matheuszordan97@gmail.com>
2024-01-24 18:12:15 -0300

    exercicios

--- exercicios/aula11/ex008.js
+++ exercicios/aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('o console funcionou corretamente!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel >= 60) {
+    console.log(`Você ulrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Boa viagem!`)
