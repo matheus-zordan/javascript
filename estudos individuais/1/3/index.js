5c9d707b14a78a97be998b01afe74525685399af
Matheus de Assis Zordan <matheuszordan97@gmail.com>
2024-02-26 16:37:50 -0300

    estudando js

---
+++ estudos individuais/1/3/index.js
@@ -0,0 +1,11 @@
+var palavra = '        Olá Mundo!'
+console.log(palavra.toUpperCase())
+console.log(palavra.toLowerCase())
+console.log(palavra.trim())
+
+var numero = 10.874023
+console.log(numero.toFixed(2))
+var numero2 = 12321423532
+console.log(numero2.toLocaleString())
+var num= parseInt("3,14")
+console.log(num)
