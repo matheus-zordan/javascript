1b594ad3ae5ed8cb00221f4397d838a167a89804
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-12-02 10:00:07 -0300

    Add console logging examples and styling in script.js

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,24 @@
+//This a single line of code
+
+/*
+console.log(100);
+
+console.log("Hello World");
+
+console.log(20, "Hello World", true);
+*/
+
+const x = 100;
+
+console.log(x);
+
+console.table({ name: "Matheus", age: 27 });
+
+console.group("simple");
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
+
+const styles = "padding: 10px; background-color: white; color:green";
+
+console.log('%cTestando Estilos',styles)
