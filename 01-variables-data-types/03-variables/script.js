38472344026c5fb87258e70c7afa540ed221ccf3
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-12-02 10:23:58 -0300

    Add variable declarations and console logging examples in script.js

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -1,5 +1,13 @@
 // Ways to declare a variable
 // var, let, const
+let firstName = "Matheus";
+let lastName = "Zordan";
+
+console.log(firstName, lastName);
+
+var age = 27;
+
+console.log(age);
 
 // Naming Conventions
 // - Only letters, numbers, underscores and dollar signs
@@ -10,3 +18,42 @@
 // first_name    underscore
 // FirstName     PascalCase
 // firstname     owercase
+
+//Re-assigning Variables
+
+age = 31;
+console.log(age);
+
+let score;
+score = 1;
+
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+console.log(score);
+
+const x = 100;
+
+const arr = [1, 2, 3];
+
+arr.push(4);
+
+console.log(arr);
+
+const person = {
+  name: "Matheus",
+};
+
+person.name = "Zeca";
+person.age = 60;
+
+console.log(person);
+//Declare multiple values at once
+
+let a, b, c;
+
+const d = 10,
+  e = 5,
+  f = 8;
