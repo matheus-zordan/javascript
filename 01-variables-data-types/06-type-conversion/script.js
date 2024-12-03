393483c90f04e12bd772b9b61e11244493c204b4
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-12-03 09:46:19 -0300

    Add type conversion examples in script.js

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,19 @@
+let amount = 1;
+
+//String to number
+//amount = parseInt(amount);
+//amount = +amount
+//amount = Number(amount)
+
+//Number to String
+
+//amount = amount.toString()
+//amount = String(amount);
+
+//String to decimal
+//amount = parseFloat(amount);
+
+//Number to boolean
+//amount = Boolean(amount);
+
+console.log(amount, typeof amount);
