257c2845ee425a933303bda9b7ab92aeaa86e625
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-12-03 08:09:40 -0300

    Add variable declarations and data types examples in script.js

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,33 @@
+//String
+const fristName = "Matheus";
+
+//Number
+const age = 27;
+const temp = 27.5;
+
+//Bolean
+const hasKids = true;
+
+//Null
+const aptNumber = null;
+
+//Undefined
+let score;
+
+//BigInt
+const n = 946464653643n;
+
+//Reference Types
+const numbers = [1, 2, 3, 4];
+
+const person = {
+  name: "Matheus",
+};
+
+function sayHello() {
+  console.log("Hello");
+}
+
+const output = temp;
+
+console.log(output, typeof output);
