1c2a3eab429c06453adc05667be5da5b84ec3be9
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-12-07 15:40:52 -0300

    Add examples of arithmetic and assignment operators in script.js

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -1,7 +1,45 @@
 // 1. Arithmetic Operators
 
+let x;
 
+x = 1 + 1;
+x = 1 - 1;
+x = 1 * 1;
+x = 1 / 1;
+x = 1 % 1;
+
+//Concatenation
+
+x = "Hello" + "World";
+
+//Expoente
+
+x = 2 ** 2;
+
+//Increment
+x = 2;
+x++;
+
+//Decrement
+x = 2;
+x--;
 // 2. Assignment Operators
 
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
 
 // 3. Comparison Operators
+x = 2 == 2;
+x = 2 === 2;
+x = 2 != 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
