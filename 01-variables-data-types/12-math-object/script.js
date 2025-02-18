8634f13f14400848a80231701b0830433c9fd47c
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2025-02-18 10:12:07 -0500

    Add examples of Math object methods in script.js

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,14 @@
+let x;
+
+x = Math.sqrt(9); //raiz quadrada
+x = Math.abs(-5); //numero absoluto
+x = Math.round(4.2); //arredonda de acordo com o numero
+x = Math.floor(8.4); //arredonda para baixo idependente do valor
+x = Math.ceil(8.4); //arredonda para cima idependente do valor
+x = Math.pow(2, 3);
+x = Math.min(1, 2, 3);
+x = Math.max(5, 90, 421);
+x = Math.random();
+x = Math.floor(Math.random() * 100);
+
+console.log(x);
