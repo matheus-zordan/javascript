264316eaaf357a2f62845226501a6786c9b2dac6
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2025-02-18 09:42:59 -0500

    Add examples of number methods and conversions in script.js

---
+++ 01-variables-data-types/11-numbers/script.js
@@ -0,0 +1,10 @@
+let x;
+
+const num = new Number(5.7985);
+
+x = num.toString();
+x = num.toString().length;
+x = num.toFixed(2);
+x = num.toPrecision(3);
+x = num.toExponential(5)
+console.log(x);
